9e25840578f1fae6320c39e7644540fe94af2bbc
Allen Gregory <allengregoryiv@gmail.com>
2022-09-26 07:16:00 -0500

    fix typo

--- src/main/java/frc/SpectrumLib/telemetry/TelemetrySubsystem.java
+++ src/main/java/frc/SpectrumLib/telemetry/TelemetrySubsystem.java
@@ -4,25 +4,14 @@
 
 package frc.SpectrumLib.telemetry;
 
-import edu.wpi.first.util.sendable.Sendable;
-import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TelemetrySubsystem extends SubsystemBase {
     protected ShuffleboardTab tab;
 
     public TelemetrySubsystem(String name) {
-        tab = Shuffleboard.getTab("name");
-    }
-
-    protected SimpleWidget addWidget(String name, Object data) {
-        return tab.add(name, data);
-    }
-
-    protected ComplexWidget addWidget(String name, Sendable sendable) {
-        return tab.add(name, sendable);
+        tab = Shuffleboard.getTab(name);
     }
 }
