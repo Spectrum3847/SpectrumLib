51df25d59bdb7f28f3717a65f325c72d2f10876b
Allen Gregory <allengregoryiv@gmail.com>
2022-09-28 19:53:35 -0500

    minor updates

--- src/main/java/frc/SpectrumLib/subsystems/MotorSubsystem.java
+++ src/main/java/frc/SpectrumLib/subsystems/MotorSubsystem.java
@@ -1,10 +1,7 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.SpectrumLib.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -28,6 +25,17 @@ public class MotorSubsystem extends SubsystemBase {
         motorLeader.setNeutralMode(config.kNeutralMode);
     }
 
+    public void setupFalconFollower(TalonFX motorFollower) {
+        setupFalconFollower(motorFollower, motorLeader, config.kFollowerInverted);
+    }
+
+    public void setupFalconFollower(TalonFX motorFollower, TalonFX motorLeader, boolean inverted) {
+        TalonFXSetup.configFollowerSetup(motorFollower, config.TalonFXConfig);
+        motorFollower.setInverted(inverted);
+        motorFollower.setNeutralMode(config.kNeutralMode);
+        motorFollower.follow(motorLeader);
+    }
+
     public void setManualOutput(double speed) {
         motorLeader.set(speed);
     }
@@ -42,6 +50,10 @@ public class MotorSubsystem extends SubsystemBase {
 
     public void resetEncoder() {
         motorLeader.setSelectedSensorPosition(0);
+    }
+
+    public double getCurrent() {
+        return motorLeader.getSupplyCurrent();
     }
 
     public void setBrakeMode(boolean mode) {
@@ -61,30 +73,6 @@ public class MotorSubsystem extends SubsystemBase {
     // Get Position
     public double getPosition() {
         return motorLeader.getSelectedSensorPosition();
-    }
-
-    public double getPercentOutput() {
-        return motorLeader.getMotorOutputPercent();
-    }
-
-    public double getOutputVoltage() {
-        return motorLeader.getMotorOutputVoltage();
-    }
-
-    public double getSupplyCurrent() {
-        return motorLeader.getSupplyCurrent();
-    }
-
-    public double getStatorCurrent() {
-        return motorLeader.getStatorCurrent();
-    }
-
-    public double getTemp() {
-        return motorLeader.getTemperature();
-    }
-
-    public double getFirmwareVersion() {
-        return motorLeader.getFirmwareVersion();
     }
 
     // Set Encoder
