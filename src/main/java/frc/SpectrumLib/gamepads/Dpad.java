117ae8fc9f22a4f2a88e326c6150319603a6e283
Allen Gregory <allengregoryiv@gmail.com>
2022-11-18 02:21:50 -0600

    Dpad to triggers instead of  AxisButtons

--- src/main/java/frc/SpectrumLib/gamepads/Dpad.java
+++ src/main/java/frc/SpectrumLib/gamepads/Dpad.java
@@ -2,34 +2,47 @@
 package frc.SpectrumLib.gamepads;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.SpectrumLib.gamepads.AxisButton.ThresholdType;
 import frc.SpectrumLib.gamepads.XboxGamepad.XboxAxis;
 import frc.SpectrumLib.gamepads.XboxGamepad.XboxDpad;
 
 public class Dpad {
     public final Joystick joy;
-    public AxisButton Up;
-    public AxisButton Down;
-    public AxisButton Left;
-    public AxisButton Right;
-    public AxisButton UpLeft;
-    public AxisButton UpRight;
-    public AxisButton DownLeft;
-    public AxisButton DownRight;
+    public Trigger Up;
+    public Trigger Down;
+    public Trigger Left;
+    public Trigger Right;
+    public Trigger UpLeft;
+    public Trigger UpRight;
+    public Trigger DownLeft;
+    public Trigger DownRight;
 
     public Dpad(Joystick joystick) {
         this.joy = joystick;
-        this.Up = new AxisButton(joy, XboxAxis.DPAD, XboxDpad.UP.value, ThresholdType.POV);
-        this.Down = new AxisButton(joy, XboxAxis.DPAD, XboxDpad.DOWN.value, ThresholdType.POV);
-        this.Left = new AxisButton(joy, XboxAxis.DPAD, XboxDpad.LEFT.value, ThresholdType.POV);
-        this.Right = new AxisButton(joy, XboxAxis.DPAD, XboxDpad.RIGHT.value, ThresholdType.POV);
-        this.UpLeft = new AxisButton(joy, XboxAxis.DPAD, XboxDpad.UP_LEFT.value, ThresholdType.POV);
+        this.Up =
+                new AxisButton(joy, XboxAxis.DPAD, XboxDpad.UP.value, ThresholdType.POV).trigger();
+        this.Down =
+                new AxisButton(joy, XboxAxis.DPAD, XboxDpad.DOWN.value, ThresholdType.POV)
+                        .trigger();
+        this.Left =
+                new AxisButton(joy, XboxAxis.DPAD, XboxDpad.LEFT.value, ThresholdType.POV)
+                        .trigger();
+        this.Right =
+                new AxisButton(joy, XboxAxis.DPAD, XboxDpad.RIGHT.value, ThresholdType.POV)
+                        .trigger();
+        this.UpLeft =
+                new AxisButton(joy, XboxAxis.DPAD, XboxDpad.UP_LEFT.value, ThresholdType.POV)
+                        .trigger();
         this.UpRight =
-                new AxisButton(joy, XboxAxis.DPAD, XboxDpad.UP_RIGHT.value, ThresholdType.POV);
+                new AxisButton(joy, XboxAxis.DPAD, XboxDpad.UP_RIGHT.value, ThresholdType.POV)
+                        .trigger();
         this.DownLeft =
-                new AxisButton(joy, XboxAxis.DPAD, XboxDpad.DOWN_LEFT.value, ThresholdType.POV);
+                new AxisButton(joy, XboxAxis.DPAD, XboxDpad.DOWN_LEFT.value, ThresholdType.POV)
+                        .trigger();
         this.DownRight =
-                new AxisButton(joy, XboxAxis.DPAD, XboxDpad.DOWN_RIGHT.value, ThresholdType.POV);
+                new AxisButton(joy, XboxAxis.DPAD, XboxDpad.DOWN_RIGHT.value, ThresholdType.POV)
+                        .trigger();
     }
 
     public double getValue() {
